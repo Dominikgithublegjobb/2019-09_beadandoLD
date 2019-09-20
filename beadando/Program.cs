496bbae741ec60a07b19adc02358ad22deb3bc74
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 12:10:36 +0200

    1.feladat

--- beadando/Program.cs
+++ beadando/Program.cs
@@ -8,7 +8,13 @@ namespace beadando {
     class Program {
         static void Main(string[] args) {
             int i = 0;
+
+            int db = 0;
+            
             List<int> szamok = new List<int>();
+            Console.WriteLine("hany darab szamot akar megadni? ");
+            db= int.Parse(Console.ReadLine());
+
             do {
                 Console.WriteLine("adjon meg szamot: ");
                 int szam1 = int.Parse(Console.ReadLine());
@@ -16,7 +22,7 @@ namespace beadando {
                 i++;
 
             }
-            while (i < 10);
+            while (i < db);
             Console.WriteLine("a legnagyobb szam: "+szamok.Max());
             Console.ReadKey();
 
