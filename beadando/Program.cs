bd2e097b60b097844e623789e8ea514c1dc367c2
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 12:05:52 +0200

    alap bekeres

--- beadando/Program.cs
+++ beadando/Program.cs
@@ -7,6 +7,22 @@ using System.Threading.Tasks;
 namespace beadando {
     class Program {
         static void Main(string[] args) {
+            int i = 0;
+            List<int> szamok = new List<int>();
+            do {
+                Console.WriteLine("adjon meg szamot: ");
+                int szam1 = int.Parse(Console.ReadLine());
+                szamok.Add(szam1);
+                i++;
+
+            }
+            while (i < 10);
+            Console.WriteLine("a legnagyobb szam: "+szamok.Max());
+            Console.ReadKey();
+
+               
+            
+            
         }
     }
 }
