e68ed7a8aa903161b0534d0d55872e5eb5458b7a
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 12:36:07 +0200

    4.feladat

--- beadando/Program.cs
+++ beadando/Program.cs
@@ -13,19 +13,42 @@ namespace beadando {
             
             List<int> szamok = new List<int>();
             Console.WriteLine("hany darab szamot akar megadni? ");
-            db= int.Parse(Console.ReadLine());
+            if (int.TryParse(Console.ReadLine(), out db)) {
+                // its a number
+                db += db;
+            }
+            else {
+                // it isn't a number
+                Console.WriteLine("ez nem szam! ");
+                Console.ReadKey();
+                System.Environment.Exit(0);
+            }
+
 
             do {
-                Console.WriteLine("adjon meg szamot: ");
-                int szam1 = int.Parse(Console.ReadLine());
-                szamok.Add(szam1);
-                i++;
 
-            }
-            while (i < db);
-            Console.WriteLine("a legnagyobb szam: "+szamok.Max());
-            Console.WriteLine("a legkisseb szam: "+szamok.Min());
-            Console.ReadKey();
+
+                Console.WriteLine("adjon meg szamot: ");
+                int szam1 = 0;
+                if (int.TryParse(Console.ReadLine(), out szam1)) {
+                    // its a number
+                    szam1 = szam1;
+                }
+                else {
+                    // it isn't a number
+                    Console.WriteLine("ez nem szam! ");
+                    System.Environment.Exit(0);
+                }
+                
+                    szamok.Add(szam1);
+                    i++;
+
+                }
+                while (i < (db/2));
+                Console.WriteLine("a legnagyobb szam: " + szamok.Max());
+                Console.WriteLine("a legkisseb szam: " + szamok.Min());
+                Console.ReadKey();
+           
 
                
             
